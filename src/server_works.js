94e0e38834c278ecd1cdda0e0e88adca42481d57
Jaeyoung Kim <50346571+Jaeyoung-Kim-CND@users.noreply.github.com>
2020-07-27 03:48:54 -0600

    __final

--- src/server_works.js
+++ src/server_works.js
@@ -4,13 +4,10 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const mongoose = require('mongoose')
-const moment = require('moment');
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
-
-moment.locale();
 
 mongoose.Promise = Promise
 
@@ -38,16 +35,14 @@ app.get('/messages/:user', (req, res) => {
 app.post('/messages', async (req, res) => {
     try {
         const message = new Message(req.body);
-        message.date = moment(new Date()).format('MM/DD/YYYY h:mm:ss a')
-
         const savedMessage = await message.save();
-        console.log('saved - ', savedMessage)
+        console.log('saved')
         const censored = await Message.findOne({message: 'badword'});  // to filter bad words
 
         if (censored)
             await Message.remove({_id: censored.id})
         else {
-            io.emit('message', message) // send 'req.body' to 'socket.on('message', addMessages)' to the client(index.html)
+            io.emit('message', req.body) // send 'req.body' to 'socket.on('message', addMessages)' to the client(index.html)
         }
 
         res.sendStatus(200)
@@ -59,62 +54,62 @@ app.post('/messages', async (req, res) => {
     }
 })
 
-app.post('/reset', async (req, res) => {
+app.post('/reset', async (req, res) => { // todo: 리셋 후에 새로 고침이 안됨.
     try {
         await mongoose.connection.db.dropCollection('messages');
         const defaultChat = [
             {
                 name: "RUTH",
                 message: "Tell us of the accommodations in steerage, Mr. Dawson. I hear they're quite good on this ship.",
-                date: "04/14/1912 07:21 PM"
+                date: "04-14-1912 19:21"
             },
             {
                 name: "JACK",
                 message: "The best I've seen, m'am. Hardly any rats.",
-                date: "04/14/1912 07:21 PM"
+                date: "04-14-1912 19:21"
             },
             {
                 name: "CAL",
                 message: "Mr. Dawson is joining us from third class. He was of some assistance to my fiancee last night.\n" +
                     "This is foie gras. It's goose liver.",
-                date: "04/14/1912 07:21 PM"
+                date: "04-14-1912 19:21"
             },
             {
                 name: "GUGGENHEIM",
                 message: "What is Hockly hoping to prove, bringing this... bohemian... up here?",
-                date: "04/14/1912 07:21 PM"
+                date: "04-14-1912 19:21"
             },
             {
                 name: "WAITER",
                 message: "How do you take your caviar, sir?",
-                date: "04/14/1912 07:21 PM"
+                date: "04-14-1912 19:21"
             },
             {
                 name: "CAL",
                 message: "Just a soupcon of lemon...\n" +
                     "...it improves the flavor with champagne.",
-                date: "04/14/1912 07:21 PM"
+                date: "04-14-1912 19:21"
             },
             {
                 name: "JACK",
                 message: "No caviar for me, thanks.\n" +
                     "Never did like it much.",
-                date: "04/14/1912 07:21 PM"
+                date: "04-14-1912 19:21"
             },
             {
                 name: "RUTH",
                 message: "And where exactly do you live, Mr.Dawson?",
-                date: "04/14/1912 07:22 PM"
+                date: "04-14-1912 19:22"
             },
             {
                 name: "JACK",
                 message: "Well, right now my address is the RMS Titanic. After that, I'm on God's good humor.",
-                date: "04/14/1912 07:22 PM"
+                date: "04-14-1912 19:22"
             },
             {
                 name: "RUTH",
                 message: "You find that sort of rootless existence appealing, do you?",
-                date: "04/14/1912 07:22 PM"
+                date: "04-14-1912 19:22"
             },
             {
                 name: "JACK",
@@ -125,22 +120,22 @@ app.post('/reset', async (req, res) => {
                     "See, my folks died in a fire when I was fifteen, and I've been on the road since.\n" +
                     "Something like that teaches you to take life as it comes at you.\n" +
                     "To make each day count.",
-                date: "04/14/1912 07:22 PM"
+                date: "04-14-1912 19:22"
             },
             {
                 name: "MOLLY",
                 message: "Well said, Jack.",
-                date: "04/14/1912 07:22 PM"
+                date: "04-14-1912 19:22"
             },
             {
                 name: "ROSE",
                 message: "To making it count.",
-                date: "04/14/1912 07:23 PM"
+                date: "04-14-1912 19:23"
             },
             {
                 name: "RUTH",
                 message: "How is it you have the means to travel, Mr. Dawson?",
-                date: "04/14/1912 07:23 PM"
+                date: "04-14-1912 19:23"
             },
             {
                 name: "JACK",
@@ -148,17 +143,17 @@ app.post('/reset', async (req, res) => {
                     "Tramp steamers and such.\n" +
                     "I won my ticket on Titanic here in a lucky hand at poker.\n" +
                     "A very lucky hand.",
-                date: "04/14/1912 07:23 PM"
+                date: "04-14-1912 19:23"
             },
             {
                 name: "GRACIE",
                 message: "All life is a game of luck.",
-                date: "04/14/1912 07:23 PM"
+                date: "04-14-1912 19:23"
             },
             {
                 name: "CAL",
                 message: "A real man makes his own luck, Archie.",
-                date: "04/14/1912 07:23 PM"
+                date: "04-14-1912 19:23"
             },
             {
                 name: "ROSE",
@@ -166,33 +161,33 @@ app.post('/reset', async (req, res) => {
                     "I see you everywhere writing in this little book.\n" +
                     "Increase number of screws in hat hooks from 2 to 3.\n" +
                     "You build the biggest ship in the world and this preoccupies you?!",
-                date: "04/14/1912 07:24 PM"
+                date: "04-14-1912 19:24"
             },
             {
                 name: "ISMAY",
                 message: "He knows every rivet in her, don't you Thomas?",
-                date: "04/14/1912 07:24 PM"
+                date: "04-14-1912 19:24"
             },
             {
                 name: "ANDREWS",
                 message: "All three million of them.",
-                date: "04/14/1912 07:24 PM"
+                date: "04-14-1912 19:24"
             },
             {
                 name: "ISMAY",
                 message: "His blood and soul are in the ship.\n" +
                     "She may be mine on paper, but in the eyes of God she belongs to Thomas Andrews.",
-                date: "04/14/1912 07:24 PM"
+                date: "04-14-1912 19:24"
             },
             {
                 name: "ROSE",
                 message: "Your ship is a wonder, Mr. Andrews. Truly.",
-                date: "04/14/1912 07:24 PM"
+                date: "04-14-1912 19:24"
             },
             {
                 name: "ANDREWS",
                 message: "Thank you, Rose.",
-                date: "04/14/1912 07:24 PM"
+                date: "04-14-1912 19:24"
             },
         ]
 
@@ -210,7 +205,7 @@ app.post('/reset', async (req, res) => {
     }
 })
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('a user connected')
 })
 
