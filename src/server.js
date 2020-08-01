7e630c0cac03008f6098597b0ead500b7bb8bd49
Jaeyoung Kim <50346571+Jaeyoung-Kim-CND@users.noreply.github.com>
2020-08-01 00:13:24 -0600

    aws testing

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ const Message = mongoose.model('Message', {
 })
 
 io.on('connection', () => {
-    console.log('a user connected.')
+    console.log('a user connected')
 })
 
 mongoose.connect(dbUrl, {useUnifiedTopology: true, useNewUrlParser: true}, (err) => {
@@ -71,10 +71,9 @@ app.post('/messages', async (req, res) => {
 
 app.post('/reset', async (req, res) => {
     try {
-        console.log("hello")
-        /*await mongoose.connection.db.dropCollection('messages');  //drop the collection 'messages'
+        await mongoose.connection.db.dropCollection('messages');  //drop the collection 'messages'
         await Message.collection.insertMany(defaultChat, {ordered: true});  //load dafaultChat and insert into the collection 'messages'
-        io.emit('refreshScreen') // send it to 'socket.on('refreshScreen', ...)' to the client(index.html)*/
+        io.emit('refreshScreen') // send it to 'socket.on('refreshScreen', ...)' to the client(index.html)
     } catch (error) {
         res.sendStatus(500)
         return console.error(error)
@@ -83,9 +82,9 @@ app.post('/reset', async (req, res) => {
     }
 })
 
-app.get('*', (req, res) => {  // to deploy (frontend + backend)
+/*app.get('*', (req, res) => {  // to deploy (frontend + backend)
     res.sendFile(path.join(__dirname + '/index.html'));
-})
+})*/
 
 const server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
